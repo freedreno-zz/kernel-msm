fcad6021f18217cb60e879a40b29287304185b04
Syed Rameez Mustafa <rameezmustafa@codeaurora.org>
2012-07-19 14:31:34 -0700

    msm: 8064: Remove cpu dcvs governor from device list
    8064 does not support the msm dcvs governor
    
    Change-Id: I51d44cf7d3339127f86b736e0c9b7c18122830f2
    CRs-Fixed: 363761
    Signed-off-by: Syed Rameez Mustafa <rameezmustafa@codeaurora.org>
    (cherry picked from commit 92b36039364eec4798c786abc8c5c1cee8c22870)
    
    Signed-off-by: Dhivya Subramanian <dthiru@codeaurora.org>

--- arch/arm/mach-msm/board-8064.c
+++ arch/arm/mach-msm/board-8064.c
@@ -2326,7 +2326,6 @@ static struct platform_device *common_devices[] __initdata = {
 	&msm_gss,
 	&apq8064_rtb_device,
 	&apq8064_cpu_idle_device,
-	&apq8064_msm_gov_device,
 	&apq8064_device_cache_erp,
 	&epm_adc_device,
 	&apq8064_qdss_device,
