177820baa2b0edcdaca22afb1d09b9b68336ff53
Aviral Gupta <aviralg@codeaurora.org>
2012-11-18 17:24:50 +0530

    ASoC: msm: Moving the gpio request and free in init and exit.
    For every session startup and end the gpio request and the gpio
    free occurs.
    Moving the gpio request to init and gpio free to remove function
    respectively to avoid the overhead of request and free for every
    session established and tear downed.
    
    CRs-Fixed: 405837
    Signed-off-by: Aviral Gupta <aviralg@codeaurora.org>
    (cherry picked from commit f964d009d50b0d693175e2696cc6ca3f1998de8f)
    
    Change-Id: I937785f4e791de462c8d54d3abf696cfb39e68d1
    Signed-off-by: Sridhar Gujje <sgujje@codeaurora.org>
    Signed-off-by: Aviral Gupta <aviralg@codeaurora.org>

--- sound/soc/msm/mpq8064.c
+++ sound/soc/msm/mpq8064.c
@@ -936,7 +936,6 @@ static void msm_mi2s_shutdown(struct snd_pcm_substream *substream)
 		clk_put(mi2s_bit_clk);
 		mi2s_bit_clk = NULL;
 	}
-	msm_mi2s_free_gpios();
 }
 
 static int configure_mi2s_gpio(void)
@@ -969,7 +968,6 @@ static int msm_mi2s_startup(struct snd_pcm_substream *substream)
 	int ret = 0;
 	struct snd_soc_pcm_runtime *rtd = substream->private_data;
 	struct snd_soc_dai *cpu_dai = rtd->cpu_dai;
-	configure_mi2s_gpio();
 	mi2s_bit_clk = clk_get(cpu_dai->dev, "bit_clk");
 	if (IS_ERR(mi2s_bit_clk))
 		return PTR_ERR(mi2s_bit_clk);
@@ -1149,7 +1147,6 @@ static void mpq8064_sec_i2s_rx_shutdown(struct snd_pcm_substream *substream)
 			clk_put(sec_i2s_rx_osr_clk);
 			sec_i2s_rx_osr_clk = NULL;
 		}
-		mpq8064_sec_i2s_rx_free_gpios();
 	}
 	pr_info("%s(): substream = %s  stream = %d\n", __func__,
 		 substream->name, substream->stream);
@@ -1188,7 +1185,6 @@ static int mpq8064_sec_i2s_rx_startup(struct snd_pcm_substream *substream)
 	struct snd_soc_pcm_runtime *rtd = substream->private_data;
 	struct snd_soc_dai *cpu_dai = rtd->cpu_dai;
 	if (substream->stream == SNDRV_PCM_STREAM_PLAYBACK) {
-		configure_sec_i2s_rx_gpio();
 		sec_i2s_rx_osr_clk = clk_get(cpu_dai->dev, "osr_clk");
 		if (IS_ERR(sec_i2s_rx_osr_clk)) {
 			pr_err("Failed to get sec_i2s_rx_osr_clk\n");
@@ -1764,6 +1760,8 @@ static int __init msm_audio_init(void)
 	} else
 		msm_headset_gpios_configured = 1;
 
+	configure_sec_i2s_rx_gpio();
+	configure_mi2s_gpio();
 	return ret;
 
 }
@@ -1776,6 +1774,8 @@ static void __exit msm_audio_exit(void)
 		return ;
 	}
 	msm_free_headset_mic_gpios();
+	mpq8064_sec_i2s_rx_free_gpios();
+	msm_mi2s_free_gpios();
 	platform_device_unregister(msm_snd_device);
 	kfree(mbhc_cfg.calibration);
 }
