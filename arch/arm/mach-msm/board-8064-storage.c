07c4310e9d71defcf433d5eabbc0d594d30272f2
Ranjith Kagathi Ananda <rkagat@codeaurora.org>
2013-01-15 17:06:49 -0800

    msm: board8064-storage: SD card drive strength reduced to 4ma
    On MPQ-DMA, since the routing lines are shorter, the higher
    sdcard drive is causing reflections, hence drive strength
    reduced from 8ma to 4ma
    
    Change-Id: If9ffb577e1d7710b83df18b6e885409394a68483
    Signed-off-by: Ranjith Kagathi Ananda <rkagat@codeaurora.org>
    
    Conflicts:
    
    	arch/arm/mach-msm/board-8064-storage.c
    
    Signed-off-by: Ranjith Kagathi Ananda <rkagat@codeaurora.org>

--- arch/arm/mach-msm/board-8064-storage.c
+++ arch/arm/mach-msm/board-8064-storage.c
@@ -339,12 +339,18 @@ void __init apq8064_init_mmc(void)
 		apq8064_add_sdcc(2, apq8064_sdc2_pdata);
 
 	if (apq8064_sdc3_pdata) {
+		if (machine_is_mpq8064_hrd() || machine_is_mpq8064_dtv() ||
+			machine_is_mpq8064_dma()) {
+			apq8064_sdc3_pdata->uhs_caps &= ~(MMC_CAP_UHS_SDR12 |
+				MMC_CAP_UHS_SDR25 | MMC_CAP_UHS_DDR50 |
+				MMC_CAP_UHS_SDR50 | MMC_CAP_UHS_SDR104);
+		}
 		if (!machine_is_apq8064_cdp()) {
 			apq8064_sdc3_pdata->wpswitch_gpio = 0;
 			apq8064_sdc3_pdata->is_wpswitch_active_low = false;
 		}
 		if (machine_is_mpq8064_cdp() || machine_is_mpq8064_hrd() ||
-			machine_is_mpq8064_dtv()) {
+			machine_is_mpq8064_dtv() || machine_is_mpq8064_dma()) {
 			int rc;
 			struct pm_gpio sd_card_det_init_cfg = {
 				.direction      = PM_GPIO_DIR_IN,
@@ -378,6 +384,24 @@ void __init apq8064_init_mmc(void)
 			     i++)
 				apq8064_sdc3_pdata->pin_data->pad_data->\
 					drv->on[i].val = GPIO_CFG_10MA;
+		}
+		if (machine_is_mpq8064_hrd() || machine_is_mpq8064_dtv()) {
+			apq8064_sdc3_pdata->pin_data->pad_data->\
+				drv->on[0].val = GPIO_CFG_16MA;
+			apq8064_sdc3_pdata->pin_data->pad_data->\
+				drv->on[1].val = GPIO_CFG_10MA;
+			apq8064_sdc3_pdata->pin_data->pad_data->\
+				drv->on[2].val = GPIO_CFG_10MA;
+		}
+		if (machine_is_mpq8064_dma()) {
+			int i;
+
+                        for (i = 0;
+                             i < apq8064_sdc3_pdata->pin_data->pad_data->\
+                                 drv->size;
+                             i++)
+                                apq8064_sdc3_pdata->pin_data->pad_data->\
+                                        drv->on[i].val = GPIO_CFG_4MA;
 		}
 		apq8064_add_sdcc(3, apq8064_sdc3_pdata);
 	}
