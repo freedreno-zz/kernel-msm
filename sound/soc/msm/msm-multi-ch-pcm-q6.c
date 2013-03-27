c492191cd5d54673e3032f3d7396b456670f7609
Santosh Mardi <gsantosh@codeaurora.org>
2013-03-27 15:02:33 +0530

    ASOC: msm: 24 Bit playback support
    - Added the support in driver for the format support
      and populating the correct bit_width to QDSP6 & codecs
    
    Change-Id: I7964fb09a6b45060abfe3edf64bda476136bbb02
    Signed-off-by: Santosh Mardi <gsantosh@codeaurora.org>

--- sound/soc/msm/msm-multi-ch-pcm-q6.c
+++ sound/soc/msm/msm-multi-ch-pcm-q6.c
@@ -87,7 +87,8 @@ static struct snd_pcm_hardware msm_pcm_hardware_playback = {
 				SNDRV_PCM_INFO_MMAP_VALID |
 				SNDRV_PCM_INFO_INTERLEAVED |
 				SNDRV_PCM_INFO_PAUSE | SNDRV_PCM_INFO_RESUME),
-	.formats =              SNDRV_PCM_FMTBIT_S16_LE,
+	.formats =              SNDRV_PCM_FMTBIT_S16_LE
+				| SNDRV_PCM_FMTBIT_S24_LE,
 	.rates =                SNDRV_PCM_RATE_8000_48000 | SNDRV_PCM_RATE_KNOT,
 	.rate_min =             8000,
 	.rate_max =             48000,
@@ -265,6 +266,7 @@ static int msm_pcm_playback_prepare(struct snd_pcm_substream *substream)
 	struct snd_pcm_runtime *runtime = substream->runtime;
 	struct msm_audio *prtd = runtime->private_data;
 	int ret;
+	short bit_width = 16;
 
 	pr_debug("%s\n", __func__);
 	if (prtd->mmap_flag) {
@@ -282,6 +284,7 @@ static int msm_pcm_playback_prepare(struct snd_pcm_substream *substream)
 	prtd->channel_mode = runtime->channels;
 	if (prtd->enabled)
 		return 0;
+
 	pr_debug("prtd->set_channel_map: %d", prtd->set_channel_map);
 	if (!prtd->set_channel_map) {
 		pr_debug("using default channel map");
@@ -303,8 +306,14 @@ static int msm_pcm_playback_prepare(struct snd_pcm_substream *substream)
 				prtd->channel_mode);
 		}
 	}
-	ret = q6asm_media_format_block_multi_ch_pcm(prtd->audio_client,
-			runtime->rate, runtime->channels, prtd->channel_map);
+
+	if (runtime->format == SNDRV_PCM_FORMAT_S24_LE)
+		bit_width = 24;
+
+	ret = q6asm_media_format_block_multi_ch_pcm_format_support(
+			prtd->audio_client, runtime->rate, runtime->channels,
+			prtd->channel_map, bit_width);
+
 	if (ret < 0)
 		pr_info("%s: CMD Format block failed\n", __func__);
 
