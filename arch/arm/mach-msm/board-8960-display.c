d6439b670a82a18e8523da7bf009f1a0a1a6c531
Rob Clark <robdclark@gmail.com>
2013-06-24 17:45:10 -0400

    msm: hack and slash through the tangled thicket of FB_MSM
    Conflicts:
    	arch/arm/mach-msm/board-8960.c
    	arch/arm/mach-msm/clock-8960.c

--- arch/arm/mach-msm/board-8960-display.c
+++ arch/arm/mach-msm/board-8960-display.c
@@ -676,7 +676,7 @@ static struct platform_device mipi_dsi_orise_panel_device = {
 	.id = 0,
 };
 
-#ifdef CONFIG_FB_MSM_HDMI_MSM_PANEL
+#if defined(CONFIG_FB_MSM_HDMI_MSM_PANEL) || defined(CONFIG_DRM_MSM)
 static struct resource hdmi_msm_resources[] = {
 	{
 		.name  = "hdmi_msm_qfprom_addr",
@@ -735,6 +735,7 @@ static struct platform_device wfd_device = {
 };
 #endif
 
+#if defined(CONFIG_FB_MSM_HDMI_MSM_PANEL) || defined(CONFIG_DRM_MSM)
 #ifdef CONFIG_MSM_BUS_SCALING
 static struct msm_bus_vectors dtv_bus_init_vectors[] = {
 	{
@@ -789,7 +790,6 @@ static int hdmi_panel_power(int on)
 }
 #endif
 
-#ifdef CONFIG_FB_MSM_HDMI_MSM_PANEL
 static int hdmi_enable_5v(int on)
 {
 	/* TBD: PM8921 regulator instead of 8901 */
@@ -1007,7 +1007,7 @@ void __init msm8960_init_fb(void)
 	platform_device_register(&mipi_dsi_novatek_panel_device);
 	platform_device_register(&mipi_dsi_orise_panel_device);
 
-#ifdef CONFIG_FB_MSM_HDMI_MSM_PANEL
+#if defined(CONFIG_FB_MSM_HDMI_MSM_PANEL) || defined(CONFIG_DRM_MSM)
 	platform_device_register(&hdmi_msm_device);
 #endif
 
@@ -1018,13 +1018,16 @@ void __init msm8960_init_fb(void)
 
 	msm_fb_register_device("mdp", &mdp_pdata);
 	msm_fb_register_device("mipi_dsi", &mipi_dsi_pdata);
+#if defined(CONFIG_FB_MSM_HDMI_MSM_PANEL) || defined(CONFIG_DRM_MSM)
 #ifdef CONFIG_MSM_BUS_SCALING
 	msm_fb_register_device("dtv", &dtv_pdata);
+#endif
 #endif
 }
 
 void __init msm8960_allocate_fb_region(void)
 {
+#if defined(CONFIG_FB_MSM)
 	void *addr;
 	unsigned long size;
 
@@ -1034,6 +1037,7 @@ void __init msm8960_allocate_fb_region(void)
 	msm_fb_resources[0].end = msm_fb_resources[0].start + size - 1;
 	pr_info("allocating %lu bytes at %p (%lx physical) for fb\n",
 			size, addr, __pa(addr));
+#endif
 }
 
 /**
@@ -1051,10 +1055,12 @@ static void set_mdp_clocks_for_wuxga(void)
 	mdp_1080p_vectors[0].ab = 2000000000;
 	mdp_1080p_vectors[0].ib = 2000000000;
 
+#if defined(CONFIG_FB_MSM_HDMI_MSM_PANEL) || defined(CONFIG_DRM_MSM)
 	if (hdmi_is_primary) {
 		dtv_bus_def_vectors[0].ab = 2000000000;
 		dtv_bus_def_vectors[0].ib = 2000000000;
 	}
+#endif
 }
 
 void __init msm8960_set_display_params(char *prim_panel, char *ext_panel)
