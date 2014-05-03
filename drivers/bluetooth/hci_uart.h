09ffad9e58604fecf14b137c346357a7e3f40c97
Rob Clark <robdclark@gmail.com>
2014-05-03 10:11:53 -0400

    bluetooth: fix ATH3K ID to match upstream bluez5
    This way we don't need a custom bluez userspace build.

--- drivers/bluetooth/hci_uart.h
+++ drivers/bluetooth/hci_uart.h
@@ -42,8 +42,7 @@
 #define HCI_UART_3WIRE	2
 #define HCI_UART_H4DS	3
 #define HCI_UART_LL	4
-#define HCI_UART_IBS	5
-#define HCI_UART_ATH3K	6
+#define HCI_UART_ATH3K	5
 
 #define HCI_UART_RAW_DEVICE	0
 #define HCI_UART_RESET_ON_INIT	1
