8ff12531d693f0887adfc29a3f6e56a07b7f5326
Georgi Djakov <georgi.djakov@linaro.org>
2015-07-25 12:11:28 +0200

    clk: qcom: Add support for RPM Clocks
    This patch adds initial support for clocks controlled by the RPM
    (Resource Power Manager) processor found on some Qualcomm SoCs.
    
    The RPM is a dedicated hardware engine for managing the shared
    SoC resources in order to keep the lowest power profile. It
    communicates with other hardware subsystems via shared memory
    and accepts clock requests, aggregates the requests and turns
    the clocks on/off or scales them on demand.
    
    This work is based on the codeaurora.org driver:
    https://www.codeaurora.org/cgit/quic/la/kernel/msm-3.10/tree/drivers/clk/qcom/clock-rpm.c
    
    Signed-off-by: Georgi Djakov <georgi.djakov@linaro.org>

---
+++ include/dt-bindings/clock/qcom,rpmcc-msm8974.h
@@ -0,0 +1,21 @@
+/*
+ * Copyright (C) 2015 AngeloGioacchino Del Regno <kholk11@gmail.com>
+ *
+ * May contain portions of code (C) The Linux Foundation
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef _DT_BINDINGS_CLK_MSM_RPMCC_8974_H
+#define _DT_BINDINGS_CLK_MSM_RPMCC_8974_H
+
+#define FIRST_CLOCK						0
+
+#endif
