ba90d9d9a391dc6749fc5c1e800599bc2ec584b6
Jiang Liu <jiang.liu@linux.intel.com>
2015-09-23 09:03:58 +1000

    x86, numa: kill useless code to improve code readability
    According to x86 boot sequence, early_cpu_to_node() always returns
    NUMA_NO_NODE when called from numa_init().  So kill useless code to
    improve code readability.
    
    Related code sequence as below:
    x86_cpu_to_node_map is set until step 2, so it is still the default
    value (NUMA_NO_NODE) when accessed at step 1.
    
    start_kernel()
    	setup_arch()
    		initmem_init()
    			x86_numa_init()
    				numa_init()
    					early_cpu_to_node()
    1)						return early_per_cpu_ptr(x86_cpu_to_node_map)[cpu];
    		acpi_boot_init();
    		sfi_init()
    		x86_dtb_init()
    			generic_processor_info()
    				early_per_cpu(x86_cpu_to_apicid, cpu) = apicid;
    		init_cpu_to_node()
    			numa_set_node(cpu, node);
    2)				per_cpu(x86_cpu_to_node_map, cpu) = node;
    
    	rest_init()
    		kernel_init()
    			smp_init()
    				native_cpu_up()
    					start_secondary()
    						numa_set_node()
    							per_cpu(x86_cpu_to_node_map, cpu) = node;
    
    Signed-off-by: Jiang Liu <jiang.liu@linux.intel.com>
    Cc: Mel Gorman <mgorman@suse.de>
    Cc: David Rientjes <rientjes@google.com>
    Cc: Mike Galbraith <umgwanakikbuti@gmail.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: "Rafael J . Wysocki" <rafael.j.wysocki@intel.com>
    Cc: Tang Chen <tangchen@cn.fujitsu.com>
    Cc: Tejun Heo <tj@kernel.org>
    Cc: Tony Luck <tony.luck@intel.com>
    Cc: Ingo Molnar <mingo@elte.hu>
    Cc: "H. Peter Anvin" <hpa@zytor.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- arch/x86/mm/numa.c
+++ arch/x86/mm/numa.c
@@ -593,8 +593,6 @@ static void __init numa_init_array(void)
 
 	rr = first_node(node_online_map);
 	for (i = 0; i < nr_cpu_ids; i++) {
-		if (early_cpu_to_node(i) != NUMA_NO_NODE)
-			continue;
 		numa_set_node(i, rr);
 		rr = next_node(rr, node_online_map);
 		if (rr == MAX_NUMNODES)
@@ -646,14 +644,6 @@ static int __init numa_init(int (*init_func)(void))
 	if (ret < 0)
 		return ret;
 
-	for (i = 0; i < nr_cpu_ids; i++) {
-		int nid = early_cpu_to_node(i);
-
-		if (nid == NUMA_NO_NODE)
-			continue;
-		if (!node_online(nid))
-			numa_clear_node(i);
-	}
 	numa_init_array();
 
 	return 0;
