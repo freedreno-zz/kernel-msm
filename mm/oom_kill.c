93804d07219e4955690b9df428366bb912c31126
Tetsuo Handa <penguin-kernel@i-love.sakura.ne.jp>
2015-09-23 09:04:10 +1000

    mm/oom_kill.c: fix potentially killing unrelated process
    At the for_each_process() loop in oom_kill_process(), we are comparing
    address of OOM victim's mm without holding a reference to that mm.  If
    there are a lot of processes to compare or a lot of "Kill process %d (%s)
    sharing same memory" messages to print, for_each_process() loop could take
    very long time.
    
    It is possible that meanwhile the OOM victim exits and releases its mm,
    and then mm is allocated with the same address and assigned to some
    unrelated process.  When we hit such race, the unrelated process will be
    killed by error.  To make sure that the OOM victim's mm does not go away
    until for_each_process() loop finishes, get a reference on the OOM
    victim's mm before calling task_unlock(victim).
    
    Signed-off-by: Tetsuo Handa <penguin-kernel@I-love.SAKURA.ne.jp>
    Acked-by: Michal Hocko <mhocko@suse.com>
    Cc: David Rientjes <rientjes@google.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/oom_kill.c
+++ mm/oom_kill.c
@@ -561,8 +561,9 @@ void oom_kill_process(struct oom_control *oc, struct task_struct *p,
 		victim = p;
 	}
 
-	/* mm cannot safely be dereferenced after task_unlock(victim) */
+	/* Get a reference to safely compare mm after task_unlock(victim) */
 	mm = victim->mm;
+	atomic_inc(&mm->mm_users);
 	/*
 	 * We should send SIGKILL before setting TIF_MEMDIE in order to prevent
 	 * the OOM victim from depleting the memory reserves from the user
@@ -600,6 +601,7 @@ void oom_kill_process(struct oom_control *oc, struct task_struct *p,
 		}
 	rcu_read_unlock();
 
+	mmput(mm);
 	put_task_struct(victim);
 }
 #undef K
