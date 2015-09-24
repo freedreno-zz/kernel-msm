ecf5eaf987d49808898c5447129dab7b337341fc
Rob Clark <robdclark@gmail.com>
2015-09-24 13:17:15 -0400

    hack: sync panel to version sent to list

--- drivers/gpu/drm/panel/panel-auo-novatek-1080p-vid.c
+++ drivers/gpu/drm/panel/panel-auo-novatek-1080p-vid.c
@@ -15,7 +15,6 @@
  * this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#define DEBUG 1
 #include <linux/backlight.h>
 #include <linux/gpio/consumer.h>
 #include <linux/module.h>
@@ -48,40 +47,6 @@ static inline struct auo_panel *to_auo_panel(struct drm_panel *panel)
 	return container_of(panel, struct auo_panel, base);
 }
 
-/*
-15: DCS_WRITE1
-05: DCS_WRITE
-39: DCS_LWRITE
-
-dsi_lp_mode
-somc,mdss-dsi-init-command:
-
-  D
-  T  L        W    D
-  Y  A     A  A    L
-  P  S  V  C  I    E
-  E  T  C  K  T    N
-
-  15 01 00 00 00   00 02   FF E0
-  15 01 00 00 00   00 02   FB 01
-  15 01 00 00 00   00 02   B5 86
-  15 01 00 00 00   00 02   B6 77
-  15 01 00 00 00   00 02   B8 AD
-  15 01 00 00 00   00 02   FF 20
-  15 01 00 00 00   00 02   FB 01
-  15 01 00 00 00   00 02   10 04            ; MIPI_DCS_ENTER_SLEEP_MODE
-  15 01 00 00 00   00 02   FF 24
-  15 01 00 00 00   00 02   FB 01
-  15 01 00 00 00   00 02   C6 00
-  15 01 00 00 00   00 02   C5 32
-  15 01 00 00 00   00 02   92 92
-  15 01 00 00 00   00 02   FF 10
-  15 01 00 00 00   00 02   35 00            ; MIPI_DCS_SET_TEAR_ON
-  39 01 00 00 00   00 03   44 03 00         ; MIPI_DCS_SET_TEAR_SCANLINE
-  39 01 00 00 00   00 04   3B 03 30 06
-  15 01 00 00 01   00 02   BB 10
-  05 01 00 00 1E   00 01   11               ; MIPI_DCS_EXIT_SLEEP_MODE
- */
 static int auo_panel_init(struct auo_panel *auo)
 {
 	struct mipi_dsi_device *dsi = auo->dsi;
@@ -89,107 +54,93 @@ static int auo_panel_init(struct auo_panel *auo)
 
 	dsi->mode_flags |= MIPI_DSI_MODE_LPM;
 
+	ret = mipi_dsi_generic_write(dsi, (u8[]){ 0xb0, 0x04 }, 2);
+	if (ret < 0)
+		return ret;
+
 	ret = mipi_dsi_dcs_write(dsi, 0xff, (u8[]){ 0xe0 }, 1);
-	if (ret)
+	if (ret < 0)
 		return ret;
 
 	ret = mipi_dsi_dcs_write(dsi, 0xfb, (u8[]){ 0x01 }, 1);
-	if (ret)
+	if (ret < 0)
 		return ret;
 
 	ret = mipi_dsi_dcs_write(dsi, 0xb5, (u8[]){ 0x86 }, 1);
-	if (ret)
+	if (ret < 0)
 		return ret;
 
 	ret = mipi_dsi_dcs_write(dsi, 0xb6, (u8[]){ 0x77 }, 1);
-	if (ret)
+	if (ret < 0)
 		return ret;
 
 	ret = mipi_dsi_dcs_write(dsi, 0xb8, (u8[]){ 0xad }, 1);
-	if (ret)
+	if (ret < 0)
 		return ret;
 
-	//00 02   FF 20
 	ret = mipi_dsi_dcs_write(dsi, 0xff, (u8[]){ 0x20 }, 1);
-	if (ret)
+	if (ret < 0)
 		return ret;
 
-	//00 02   FB 01
 	ret = mipi_dsi_dcs_write(dsi, 0xfb, (u8[]){ 0x01 }, 1);
-	if (ret)
+	if (ret < 0)
 		return ret;
 
-	//00 02   10 04            ; MIPI_DCS_ENTER_SLEEP_MODE
 	ret = mipi_dsi_dcs_enter_sleep_mode(dsi);
-	if (ret)
+	if (ret < 0)
 		return ret;
 
-	//00 02   FF 24
 	ret = mipi_dsi_dcs_write(dsi, 0xff, (u8[]){ 0x24 }, 1);
-	if (ret)
+	if (ret < 0)
 		return ret;
 
-	//00 02   FB 01
 	ret = mipi_dsi_dcs_write(dsi, 0xfb, (u8[]){ 0x01 }, 1);
-	if (ret)
+	if (ret < 0)
 		return ret;
 
-	//00 02   C6 00
 	ret = mipi_dsi_dcs_write(dsi, 0xc6, (u8[]){ 0x00 }, 1);
-	if (ret)
+	if (ret < 0)
 		return ret;
 
-	//00 02   C5 32
 	ret = mipi_dsi_dcs_write(dsi, 0xc5, (u8[]){ 0x32 }, 1);
-	if (ret)
+	if (ret < 0)
 		return ret;
 
-	//00 02   92 92
 	ret = mipi_dsi_dcs_write(dsi, 0x92, (u8[]){ 0x92 }, 1);
-	if (ret)
+	if (ret < 0)
 		return ret;
 
-	//00 02   FF 10
 	ret = mipi_dsi_dcs_write(dsi, 0xff, (u8[]){ 0x10 }, 1);
-	if (ret)
+	if (ret < 0)
 		return ret;
 
-	//00 02   35 00            ; MIPI_DCS_SET_TEAR_ON
 	ret = mipi_dsi_dcs_set_tear_on(dsi, MIPI_DSI_DCS_TEAR_MODE_VBLANK);
-	if (ret)
+	if (ret < 0)
 		return ret;
 
-	//00 03   44 03 00         ; MIPI_DCS_SET_TEAR_SCANLINE
 	ret = mipi_dsi_dcs_write(dsi, MIPI_DCS_SET_TEAR_SCANLINE,
 			(u8[]){ 0x03, 0x00 }, 2);
-	if (ret)
+	if (ret < 0)
 		return ret;
 
-	//00 04   3B 03 30 06
 	ret = mipi_dsi_dcs_write(dsi, 0x3b, (u8[]){ 0x03, 0x30, 0x06 }, 3);
-	if (ret)
+	if (ret < 0)
 		return ret;
 
-	//00 02   BB 10
+	// 0x10 for cmd mode, 0x03 for video mode?
 	ret = mipi_dsi_dcs_write(dsi, 0xbb, (u8[]){ 0x10 }, 1);
-	if (ret)
+	if (ret < 0)
 		return ret;
 	msleep(1);
 
-	//00 01   11               ; MIPI_DCS_EXIT_SLEEP_MODE
 	ret = mipi_dsi_dcs_exit_sleep_mode(dsi);
-	if (ret)
+	if (ret < 0)
 		return ret;
 	msleep(30);
 
 	return 0;
 }
 
-/*
-dsi_lp_mode
-qcom,mdss-dsi-on-command:
-  05 01 00 00 28   00 01  29                ; MIPI_DCS_SET_DISPLAY_ON
- */
 static int auo_panel_on(struct auo_panel *auo)
 {
 	struct mipi_dsi_device *dsi = auo->dsi;
@@ -198,7 +149,7 @@ static int auo_panel_on(struct auo_panel *auo)
 	dsi->mode_flags |= MIPI_DSI_MODE_LPM;
 
 	ret = mipi_dsi_dcs_set_display_on(dsi);
-	if (ret)
+	if (ret < 0)
 		return ret;
 
 	msleep(40);
@@ -206,13 +157,6 @@ static int auo_panel_on(struct auo_panel *auo)
 	return 0;
 }
 
-/*
-dsi_hs_mode
-qcom,mdss-dsi-off-command:
-  15 01 00 00 00   00 02   FF 10
-  05 01 00 00 00   00 01   28               ; MIPI_DCS_SET_DISPLAY_OFF
-  05 01 00 00 64   00 01   10               ; MIPI_DCS_ENTER_SLEEP_MODE
- */
 static int auo_panel_off(struct auo_panel *auo)
 {
 	struct mipi_dsi_device *dsi = auo->dsi;
@@ -221,22 +165,21 @@ static int auo_panel_off(struct auo_panel *auo)
 	dsi->mode_flags &= ~MIPI_DSI_MODE_LPM;
 
 	ret = mipi_dsi_dcs_write(dsi, 0xff, (u8[]){ 0x10 }, 1);
-	if (ret)
+	if (ret < 0)
 		return ret;
 
 	ret = mipi_dsi_dcs_set_display_off(dsi);
-	if (ret)
+	if (ret < 0)
 		return ret;
 
 	ret = mipi_dsi_dcs_enter_sleep_mode(dsi);
-	if (ret)
+	if (ret < 0)
 		return ret;
 
 	msleep(100);
 
 	return 0;
 }
-
 
 static int auo_panel_disable(struct drm_panel *panel)
 {
@@ -245,7 +188,7 @@ static int auo_panel_disable(struct drm_panel *panel)
 	if (!auo->enabled)
 		return 0;
 
-	dev_dbg(panel->dev, "disable\n");
+	DRM_DEBUG("disable\n");
 
 	if (auo->backlight) {
 		auo->backlight->props.power = FB_BLANK_POWERDOWN;
@@ -265,9 +208,7 @@ static int auo_panel_unprepare(struct drm_panel *panel)
 	if (!auo->prepared)
 		return 0;
 
-//	auo_wait_frames(auo, 4);
-
-	dev_dbg(panel->dev, "unprepare\n");
+	DRM_DEBUG("unprepare\n");
 
 	ret = auo_panel_off(auo);
 	if (ret) {
@@ -292,20 +233,20 @@ static int auo_panel_prepare(struct drm_panel *panel)
 	if (auo->prepared)
 		return 0;
 
-	dev_dbg(panel->dev, "prepare\n");
+	DRM_DEBUG("prepare\n");
+
+	if (auo->reset_gpio) {
+		gpiod_set_value(auo->reset_gpio, 0);
+		msleep(5);
+	}
 
 	ret = regulator_enable(auo->supply);
 	if (ret < 0)
 		return ret;
 
-	msleep(150);
+	msleep(20);
 
 	if (auo->reset_gpio) {
-		/* XXX reset really hard! */
-		gpiod_set_value(auo->reset_gpio, 1);
-		msleep(10);
-		gpiod_set_value(auo->reset_gpio, 0);
-		msleep(10);
 		gpiod_set_value(auo->reset_gpio, 1);
 		msleep(10);
 	}
@@ -313,21 +254,18 @@ static int auo_panel_prepare(struct drm_panel *panel)
 	msleep(150);
 
 	ret = auo_panel_init(auo);
-	if (ret < 0) {
+	if (ret) {
 		dev_err(panel->dev, "failed to init panel: %d\n", ret);
 		goto poweroff;
 	}
 
 	ret = auo_panel_on(auo);
-	if (ret < 0) {
+	if (ret) {
 		dev_err(panel->dev, "failed to set panel on: %d\n", ret);
 		goto poweroff;
 	}
 
 	auo->prepared = true;
-
-	/* wait for 6 frames before continuing */
-//	auo_wait_frames(auo, 6);
 
 	return 0;
 
@@ -345,7 +283,7 @@ static int auo_panel_enable(struct drm_panel *panel)
 	if (auo->enabled)
 		return 0;
 
-	dev_dbg(panel->dev, "enable\n");
+	DRM_DEBUG("enable\n");
 
 	if (auo->backlight) {
 		auo->backlight->props.power = FB_BLANK_UNBLANK;
@@ -424,7 +362,7 @@ static int auo_panel_add(struct auo_panel *auo)
 			PTR_ERR(auo->reset_gpio));
 		auo->reset_gpio = NULL;
 	} else {
-		gpiod_set_value(auo->reset_gpio, 0);
+		gpiod_direction_output(auo->reset_gpio, 0);
 	}
 
 	np = of_parse_phandle(dev->of_node, "backlight", 0);
@@ -519,7 +457,7 @@ static void auo_panel_shutdown(struct mipi_dsi_device *dsi)
 
 static struct mipi_dsi_driver auo_panel_driver = {
 	.driver = {
-		.name = "panel-auo-novatek-1080p-video",
+		.name = "panel-auo-novatek-1080p",
 		.of_match_table = auo_of_match,
 	},
 	.probe = auo_panel_probe,
@@ -529,5 +467,5 @@ static struct mipi_dsi_driver auo_panel_driver = {
 module_mipi_dsi_driver(auo_panel_driver);
 
 MODULE_AUTHOR("Rob Clark <robdclark@gmail.com>");
-MODULE_DESCRIPTION("AUO Novatek 1080p video mode panel driver");
+MODULE_DESCRIPTION("AUO Novatek 1080p panel driver");
 MODULE_LICENSE("GPL v2");
